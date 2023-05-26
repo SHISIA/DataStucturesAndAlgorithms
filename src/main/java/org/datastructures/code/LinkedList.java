5bbbd185ae1f24a1bdc77eaa56708e59ccc7133c
jamesshisia6@gmail.com <jamesshisia6@gmail.com>
2023-05-26 09:41:55 +0300

    Chapter 2: findAtIndex LinkedList

--- src/main/java/org/datastructures/code/LinkedList.java
+++ src/main/java/org/datastructures/code/LinkedList.java
@@ -1,6 +1,7 @@
 package org.datastructures.code;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class LinkedList<E> implements Iterable<E>, Visualizable{
 
@@ -50,5 +51,78 @@ public class LinkedList<E> implements Iterable<E>, Visualizable{
         return node;
     
     }
+
+//     Inserting an element at the beginning of a list is very similar to appending it at the
+// end. The only difference is that you need to update the first reference instead of the
+// last reference:
+    public Node<E> appendFirst(E value){
+        Node node = getNewNode();
+        node.value = value;
+        node.next=first;
+        first=node;
+        if(length==0){
+            last=node;
+        }
+        length++;
+        return node;
+
+    }
+    // Insertion at an arbitrary position
+    // can be achieved in the same way we perform an
+// insertion in the first element, except that we update the reference of the previous
+// element instead of the first reference. There is, however, a catch; we need to find the
+// position where we need to insert the element. There is no way to find it other than
+// to start at the beginning and walk all the way to the correct position while counting
+// each node we step on.
+    public Node<E> insert(int index, E value){
+        Node<E> node = getNewNode();
+
+        if(index < 0 || index > length){
+            throw new IllegalArgumentException("Invalid index for insertion");
+        }else if(index == length){
+            return appendLast(value);
+        }else if(index == 0){
+            return appendFirst(value);
+        }else {
+        //We walk all the way to the desired position while counting the
+        // nodes, or in this case, counting the index in the opposite direction:
+            Node<E> result =first;
+            while(index > 1){
+                index --;
+                result = result.next;
+            }
+            // we update the references:
+            node.value=value;
+            node.next=result.next;
+            result.next=node;
+            length++;
+            return node;
+        }
+    }
+
+    public E findAtIndex(int index){
+        //we start from the first element
+        Node<E> result = first;
+        while(index >=0)
+        {
+            if(result == null){
+                throw new NoSuchElementException();
+            }
+            //When the index is 0, we would have finally reached the desired position, so
+            // we return:
+            else if(index == 0){
+                return result.value;
+            }else{
+            // If we are not there yet, we must step onto the next element and keep counting:
+                index --;
+                result = result.next;
+            }
+        }
+        return null;
+    }
+
+    //Reoving an element in the beginning means simply updating the reference to the first element
+    //with that of the next element. NOTE: We do not update the reference in the element that has just been removed because the element along with the reference would be garbage-colected anyway
+     
     
 }
