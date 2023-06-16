4383b801886e6ca5e9e51d196ef2f78447b23ffc
SHISIA <jamesshisia6@gmail.com>
2023-06-16 17:52:51 +0300

    Search: Binary Search and sorting

---
+++ src/main/java/org/datastructures/code/Chpater_5_Efficient_Searching/Search.java
@@ -0,0 +1,39 @@
+package org.datastructures.code.Chpater_5_Efficient_Searching;
+
+public class Search {
+
+     public static void main(String[] arg) {
+        int[] numbers = {10,20,23,31,34,56,67,78,90,100};
+        System.out.println(new Search().finalsearch(numbers, 67));
+    }
+
+    public Integer finalsearch(int[] numbers,int valueToFind){
+        return search(numbers, 0, numbers.length, valueToFind);
+    }
+
+    public  Integer search(int[] values,int start, int end, int valueTofind){
+        if(start>=end){
+            return -1;
+        }
+        int midIndex = (start+end)/2;
+        int comparator = compareTo(values[midIndex], valueTofind);
+        if(comparator == 0 ){
+            return midIndex;
+        }else if(comparator > 0){
+            return search(values, start, midIndex-1, valueTofind);
+        }else{
+            return search(values, midIndex+1, end, valueTofind);
+        }
+    }
+
+    static int compareTo(int firstNum,int secondnum){
+        if(firstNum == secondnum){
+            return 0;
+        }else if(firstNum > secondnum){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+    
+}
