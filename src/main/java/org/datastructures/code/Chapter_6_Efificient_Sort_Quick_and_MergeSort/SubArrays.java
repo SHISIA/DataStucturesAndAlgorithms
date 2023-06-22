ce1550a17cff59141fefe0e1746222600bdf7e4c
SHISIA <jamesshisia6@gmail.com>
2023-06-22 09:37:19 +0300

    Subarrays

--- src/main/java/org/datastructures/code/Chapter_6_Efificient_Sort_Quick_and_MergeSort/SubArrays.java
+++ src/main/java/org/datastructures/code/Chapter_6_Efificient_Sort_Quick_and_MergeSort/SubArrays.java
@@ -1,6 +1,9 @@
 package org.datastructures.code.Chapter_6_Efificient_Sort_Quick_and_MergeSort;
 
-
+/*A subarray is a contiguous part of array. An array that is inside another array. For example, consider the array [1, 2, 3, 4],
+ There are 10 non-empty sub-arrays. The subarrays are (1), (2), (3), (4), (1,2), (2,3), (3,4), (1,2,3), (2,3,4) and (1,2,3,4).
+  In general, for an array/string of size n, there are n*(n+1)/2 non-empty subarrays/substrings.
+*/
 // Java program to generate all possible subarrays/subArrays
 //  Complexity- O(n^3) */
 public class SubArrays {
