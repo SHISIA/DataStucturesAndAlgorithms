e5f72a5faaa2f2529756c77eae4618cc3488c92b
Shisia <jamesshisia6@gmail.com>
2023-06-23 17:29:06 +0300

    Binary_Search Java : //Bro Code

---
+++ src/main/java/org/datastructures/code/Bro_Code/BinarySearch.java
@@ -0,0 +1,36 @@
+package org.datastructures.code.Bro_Code;
+
+import java.util.Arrays;
+
+/**Finds target position within a sorted array> Half of the element is eliminated during each step
+ * ADV: Efficient when working with a large data set***/
+public class BinarySearch {
+    public static void main(String[] args) {
+        int array [] =new int[100];
+        int target = 42;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+//        int index = Arrays.binarySearch(array,target);
+        int index = binarySearch(array,target);
+        if (index!=-1){
+            System.out.println("Element found at : "+index);
+        }else {
+            System.out.println("Element not found");
+        }
+    }
+
+    private static int binarySearch(int[] array, int target) {
+        int start = 0;
+        int end = array.length-1;
+        while (start <=end){
+            int middle = start + (end - start)/2;
+            int middleValue = array[middle];
+             if (middleValue < target) start = middle + 1;
+             else if (middleValue > target) end = middle - 1;
+             else return  middle;
+        }
+        return  -1;
+    }
+}
