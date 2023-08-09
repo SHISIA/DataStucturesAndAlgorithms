57df92b8da4db0c9d5bcec6eefe6a0abbb15ad9d
Jemo <jamesshisia6@gmail.com>
2023-08-09 14:17:21 +0300

    Queue: empty, poll, remove, peek,offer,add cards

--- src/main/java/stackDS/Queue.java
+++ src/main/java/stackDS/Queue.java
@@ -12,8 +12,24 @@ public class Queue {
         queue.add(card1);
         queue.add(card2);
         queue.add(card3);
-        queue.add(card4);
-        //check our present cards
+        //offer method
+        queue.offer(card4);
+        //peek method
+        System.out.println("Peek returns Card "+queue.peek().getName());
+        //poll method
+        System.out.println("Poll method returns Card "+queue.poll().getName());
+        //remove method
+        System.out.println("Remove operation removes Card "+queue.remove().getName());
+        //print our present cards
         queue.forEach(e -> System.out.println(e.getName()));
+        //check if queue is empty
+        System.out.println("Is our Queue empty ? "+ (queue.isEmpty() ? "YES" : "NO"));
+
+        //remove all elements
+        while (!queue.isEmpty()){
+            queue.remove();
+        }
+        //verify the queue is empty
+        System.out.println("Is our Queue empty ? "+ (queue.isEmpty() ? "YES" : "NO"));
     }
 }
