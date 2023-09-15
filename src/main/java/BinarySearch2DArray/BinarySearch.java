753c99a4af660464bcf3bcb35e850f2dfacb3d56
Jemo <jamesshisia6@gmail.com>
2023-09-15 09:42:30 +0300

    Binary search in 2D arrays (Matrices). Logic and example complete

--- src/main/java/BinarySearch2DArray/BinarySearch.java
+++ src/main/java/BinarySearch2DArray/BinarySearch.java
@@ -1,4 +1,36 @@
 package BinarySearch2DArray;
 
+import java.util.Arrays;
+
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
+        };
+        int[] notFound = {-1, -1};
+        int target = 33;
+        if (Arrays.equals(search(matrix, target), notFound)) {
+            System.out.println("Target not found");
+        } else {
+            System.out.println("Target found at position" + Arrays.toString(search(matrix, target)));
+        }
+    }
+
+    static int[] search(int[][] matrix, int target) {
+        int row = 0;
+        int column = 0;
+        while (row < matrix.length && column >= 0) {
+            if (matrix[row][column] == target) {
+                return new int[]{row, column};
+            } else if (matrix[row][column] < target) {
+                row++;
+            } else {
+                column--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
 }
