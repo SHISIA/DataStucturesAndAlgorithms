5f4f7ba8e52a856fc06d2db3488d151e48620311
Jemo <jamesshisia6@gmail.com>
2024-06-30 12:39:08 +0300

    Java Collections Interface (JCF) - list interface code clean-up

--- src/main/java/List_Interface_In_Java/ListClientExample.java
+++ src/main/java/List_Interface_In_Java/ListClientExample.java
@@ -1,23 +1,22 @@
 package List_Interface_In_Java;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class ListClientExample {
-    private List list;
+    private final List<?> list;
 
     /**ListClientExample constructor**/
     public ListClientExample(){
-        list= new ArrayList();
+        list= new ArrayList<>();
     }
 
-    public List getList() {
+    public List<?> getList() {
         return list;
     }
     public static void main(String[] args) {
         ListClientExample listClientExample=new ListClientExample();
-        List list1=listClientExample.getList();
+        List<?> list1=listClientExample.getList();
         System.out.println(list1);
     }
 }
